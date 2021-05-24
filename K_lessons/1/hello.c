a085fd359b788e141392e5aaf5cf514203a1670c
Isonzo <renato.soverina@gmail.com>
2021-05-24 13:06:58 -0500

    Preprocessor experiments

---
+++ K_lessons/1/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "implementation.h"
+int main()
+{
+   PrintHelloWorld();
+   return 0;
+}
---
+++ K_lessons/1/implementation.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void PrintHelloWorld() {
+  printf("Will this change?");
+}
