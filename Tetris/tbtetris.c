7dc7ab149e7a13249b6c5437c36068f62e4e3477
Isonzo <renato.soverina@gmail.com>
2021-06-12 17:47:21 -0500

    Good progress on tetris, safepoint before changing everything

---
+++ Tetris/tbtetris.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int WIDTH = 15;
+int HEIGHT = 23;
+
+char symbol = '@';
+
+char screen[23][15];
+void generatescreen()
+{
+  int i;
+  int j;
+  for(i = 0; i <= HEIGHT - 1; ++i)
+  {
+    for(j = 0; j <= WIDTH - 1; ++j)
+    {
+      if(j == 0 || j == WIDTH - 1)
+      {
+        screen[i][j] = '#';
+      }else if (i == HEIGHT - 1)
+      {
+        screen[i][j] = '#';
+      }else
+      {
+        screen[i][j] = ' ';
+      }
+    }
+  }
+}
+
+void updatescreen()
+{
+  int i;
+  int j;
+  for(i = 0; i < HEIGHT; ++i)
+  {
+    for(j = 0; j < WIDTH; ++j)
+    {
+      printf("%c", screen[i][j]);
+    }
+  printf("\n");
+  }
+}
+
+int randomint()
+{
+  int randomnumber; // I think this is skewed, something to review
+  randomnumber = rand() % 8;
+  return randomnumber;
+}
+
+void spawnpiece()
+{
+  //screen[0][WIDTH / 2] = symbol; A single dot, just for testing
+  // Randomly choose a piece to spawn
+
+  switch (randomint())
+  {
+    case 0:
+      // Hero
+      screen[0][WIDTH / 2] = symbol;
+      screen[1][WIDTH / 2] = symbol;
+      screen[2][WIDTH / 2] = symbol;
+      screen[3][WIDTH / 2] = symbol;
+      break;
+
+    case 1:
+      // Orange Ricky
+      screen[0][WIDTH / 2 - 1] = symbol;
+      screen[0][WIDTH / 2] = symbol;
+      screen[1][WIDTH / 2] = symbol;
+      screen[2][WIDTH / 2] = symbol;
+      break;
+
+    case 2:
+      // Blue Ricky
+      screen[0][WIDTH / 2 + 1] = symbol;
+      screen[0][WIDTH / 2] = symbol;
+      screen[1][WIDTH / 2] = symbol;
+      screen[2][WIDTH / 2] = symbol;
+      break;
+
+    case 3:
+    // Blue Ricky
+      screen[0][WIDTH / 2 + 1] = symbol;
+      screen[0][WIDTH / 2] = symbol;
+      screen[1][WIDTH / 2] = symbol;
+      screen[2][WIDTH / 2] = symbol;
+      break;
+
+    case 4:
+      // Cleveland Z
+      screen[0][WIDTH / 2 - 1] = symbol;
+      screen[0][WIDTH / 2] = symbol;
+      screen[1][WIDTH / 2] = symbol;
+      screen[1][WIDTH / 2 + 1] = symbol;
+      break;
+
+    case 5:
+      // Rhode Z
+      screen[0][WIDTH / 2 + 1] = symbol;
+      screen[0][WIDTH / 2] = symbol;
+      screen[1][WIDTH / 2] = symbol;
+      screen[1][WIDTH / 2 - 1] = symbol;
+      break;
+
+    case 6:
+      // Teewee
+      screen[0][WIDTH / 2] = symbol;
+      screen[1][WIDTH / 2 - 1] = symbol;
+      screen[1][WIDTH / 2] = symbol;
+      screen[1][WIDTH / 2 + 1] = symbol;
+      break;
+
+    case 7:
+      // Smashboy
+      screen[0][WIDTH / 2 + 1] = symbol;
+      screen[0][WIDTH / 2] = symbol;
+      screen[1][WIDTH / 2] = symbol;
+      screen[1][WIDTH / 2 + 1] = symbol;
+      break;
+  }
+}
+
+
+
+void physics(char dir)
+{
+  int i;
+  int j;
+  int change = 0;
+
+  for(i = HEIGHT - 1; i >=  0; --i)
+  {
+    for(j = WIDTH - 1; j >= 0; --j)
+    { //If space directly below is not a space, don't move that piece
+      if (screen[i][j] == symbol && screen[i + 1][j] == ' ')
+      {
+        screen[i][j] = ' ';
+        if (dir == 'a' && screen[i + 1][j - 1] == ' ')
+        {
+          screen[i + 1][j - 1] = symbol;
+        }else if (dir == 'd' && screen[i + 1][j + 1] == ' ')
+        {
+          screen[i + 1][j + 1] = symbol;
+        }else if (dir == 's' && screen[i + 2][j] == ' ')
+        {
+          screen[i + 2][j] = symbol;
+        }else
+        {
+          screen[i + 1][j] =symbol;
+        }
+        // Mark down that physics DID happen
+        change = 1;
+      }
+    }
+  }
+  if (!change)
+  {
+    spawnpiece();
+  }
+}
+
+int main()
+{
+  char dir;
+  generatescreen();
+  spawnpiece();
+  updatescreen();
+  while (1)
+  {
+    scanf("%c", &dir);
+    physics(dir);
+    updatescreen();
+  }
+  return 0;
+}
