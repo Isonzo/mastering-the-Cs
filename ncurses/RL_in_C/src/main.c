5402771a0f63b88d398117422d22083485417741
Isonzo <renato.soverina@gmail.com>
2021-09-17 22:42:44 -0500

    added engine.c to clean up main.c

--- ncurses/RL_in_C/src/main.c
+++ ncurses/RL_in_C/src/main.c
@@ -4,28 +4,17 @@ Entity* player;
 
 int main(void)
 {
-    int ch;
+    cursesSetup();
+
     Position start_pos = {10, 20};
-    initscr();
-    noecho();
-    curs_set(0);
+    player = createPlayer(start_pos);
 
     player = createPlayer(start_pos);
     mvaddch(player->pos.y, player->pos.x, player->ch);
 
-    while (ch = getch())
-    {
-        if (ch == 'q')
-        {
-            break;
-        }
+    gameLoop();
 
-        handleInput(ch);
-        clear();
-        mvaddch(player->pos.y, player->pos.x, player->ch);
-    }
-
-    endwin();
+    closeGame();
 
     return 0;
 }
