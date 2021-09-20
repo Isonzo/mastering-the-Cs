7ff1e92bc2a3bd2b72b43bfc6e7aa43f07180af9
Isonzo <renato.soverina@gmail.com>
2021-09-20 18:57:03 -0500

    Fixed typo in room.c, and updated .gitignore to include linux makefile

--- ncurses/RL_in_C/src/room.c
+++ ncurses/RL_in_C/src/room.c
@@ -9,12 +9,12 @@ Room createRoom(int y, int x, int height, int width)
     newRoom.height = height;
     newRoom.width = width;
     newRoom.center.y = y + (int)(height / 2);
-    newRoom.center.x = x + (int)(height / 2);
+    newRoom.center.x = x + (int)(width / 2);
 
     return newRoom;
 }
 
-void addRoomtoMap(Room room)
+void addRoomToMap(Room room)
 {
     for (int y = room.pos.y; y < room.pos.y + room.height; ++y)
     {
